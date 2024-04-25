a13ad0fe2535a96c6a9c8bc9f5482d4fd3539e26
Alejandro <alejandroacasadoc@gmail.com>
2024-04-24 21:15:42 -0400

    JWT

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Install FastAPI pip install "fastapi[all]"
 from fastapi import FastAPI
-from routers import users
+from routers import users, basic_auth_users, jwt_auth_users
 from fastapi.staticfiles import StaticFiles
 
 
@@ -9,6 +9,8 @@ app = FastAPI()
 
 # Routers
 app.include_router(users.router)
+app.include_router(basic_auth_users.router)
+app.include_router(jwt_auth_users.router)
 
 # Static files. Example http://127.0.0.1:8000/static/images/screenshot.png
 app.mount("/static", StaticFiles(directory="static"), name = "static")
